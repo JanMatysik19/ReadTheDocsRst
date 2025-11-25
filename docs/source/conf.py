1410890a8ba147eb2f050e71428f6a3d0348b5d8
JanMatysik19 <jan.matysik19@gmail.com>
2025-11-25 17:00:08 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,6 +11,13 @@ version = '0.1.0'
 
 # -- General configuration
 
+html_theme_options = {
+    'titles_only': True,
+    'navigation_depth': 1,
+    'collapse_navigation': True,
+    'includehidden': False
+}
+
 extensions = [
     'sphinx.ext.duration',
     'sphinx.ext.doctest',
